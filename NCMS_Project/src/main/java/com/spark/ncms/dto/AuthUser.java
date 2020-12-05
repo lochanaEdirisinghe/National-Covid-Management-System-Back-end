1d8f8b431ed8ec3bf2f4501eeea304e248611800
Lochana Edirisinghe <edirisinghelochana5@gmail.com>
2020-12-05 17:34:10 +0530

    small changes

--- NCMS_Project/src/main/java/com/spark/ncms/dto/AuthUser.java
+++ NCMS_Project/src/main/java/com/spark/ncms/dto/AuthUser.java
@@ -1,14 +1,48 @@
 package com.spark.ncms.dto;
 
-import lombok.*;
 
-@Getter
-@Setter
-@NoArgsConstructor
-@AllArgsConstructor
-@ToString
 public class AuthUser {
     private String userId;
     private String password;
     private String role;
+
+
+    public AuthUser(String userId, String password, String role) {
+        this.userId = userId;
+        this.password = password;
+        this.role = role;
+    }
+
+    public String getUserId() {
+        return userId;
+    }
+
+    public void setUserId(String userId) {
+        this.userId = userId;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+
+    @Override
+    public String toString() {
+        return "AuthUser{" +
+                "userId='" + userId + '\'' +
+                ", password='" + password + '\'' +
+                ", role='" + role + '\'' +
+                '}';
+    }
 }
